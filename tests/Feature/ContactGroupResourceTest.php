eeff9d51de55203bd14fb28c162858eed4b44cb7
kemal <kemalyen@gmail.com>
2024-08-08 15:16:03 +0100

    More tests are added

--- tests/Feature/ContactGroupResourceTest.php
+++ tests/Feature/ContactGroupResourceTest.php
@@ -1,20 +1,68 @@
 <?php
 
-namespace Tests\Feature;
+use App\Filament\Resources\ContactGroupResource;
+use App\Filament\Resources\ContactResource;
+use App\Filament\Resources\ServiceResource;
+use App\Models\Contact;
+use App\Models\ContactGroup;
+use App\Models\Service;
+use App\Models\Template;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
+use function Pest\Livewire\livewire;
 
-class ContactGroupResourceTest extends TestCase
-{
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
 
-        $response->assertStatus(200);
-    }
-}
+it('can render page', function () {
+    $this->get(ContactGroupResource::getUrl('index'))->assertSuccessful();
+});
+
+
+it('can create a contact group', function () {
+    $fake = fake()->name;
+    livewire(ContactGroupResource\Pages\CreateContactGroup::class)
+        ->fillForm([
+            'name' => $fake,
+
+        ])
+        ->call('create')
+        ->assertHasNoFormErrors();
+
+    $this->assertDatabaseHas(ContactGroup::class, [
+        'name' => $fake
+    ]);
+});
+
+it('can not create a contact group', function () {
+
+    livewire(ContactGroupResource\Pages\CreateContactGroup::class)
+        ->fillForm([
+            'name' => '',
+        ])
+        ->call('create')
+        ->assertHasFormErrors(['name' => 'required']);
+});
+
+
+it('can edit a contact group', function () {
+
+    $contactGroupData = ContactGroup::factory()->create();
+    $fake = fake()->name;
+    livewire(
+        ContactGroupResource\Pages\EditContactGroup::class,
+        [
+            'record' => $contactGroupData->getRouteKey()
+        ]
+    )
+        ->fillForm([
+            'name' => $fake,
+        ])
+        ->call('save')
+        ->assertHasNoErrors();
+
+    expect($contactGroupData->refresh())
+        ->name->toBe($fake);
+
+    $this->assertDatabaseHas(ContactGroup::class, [
+        'name' => $fake,
+        'id' => $contactGroupData['id']
+    ]);
+});
